b0899caff8d3acb3b0b369cf2936bf7af19318a6
sandy-sp <sandeep.paidipati@gmail.com>
2025-04-09 15:53:23 -0400

    Remove weather visualization project files and dependencies

--- projects/weather_viz/src/visualizations/plot_utils.py
+++
@@ -1,8 +0,0 @@
-def apply_common_layout(fig, title):
-    fig.update_layout(
-        title=title,
-        xaxis_tickangle=-45,
-        template="plotly_white",
-        margin=dict(t=60, b=40)
-    )
-    return fig
