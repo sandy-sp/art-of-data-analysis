ae0e52773722f2f03f2e369ad1f0f3db8b2b1a64
sandy-sp <sandeep.paidipati@gmail.com>
2025-04-09 08:31:18 -0400

    Add sidebar information section: include project details, GitHub link, and social media connections to enhance user engagement and provide additional resources.

--- projects/earthquake_viz/app/main.py
+++ projects/earthquake_viz/app/main.py
@@ -191,3 +191,37 @@ if st.sidebar.button("Fetch and Visualize Data", key="fetch_button", help="Click
 else:
     st.info("Select a country in the sidebar and click 'Fetch and Visualize Data' to load earthquake information.")
     st.markdown("---")
+
+with st.sidebar:
+    st.markdown("<div style='height: 100%; display: flex; flex-direction: column; justify-content: flex-end;'>", unsafe_allow_html=True)
+    st.markdown("""
+    <p style='margin-top: 10px; font-size: 16px;'>
+        📘 Curious how this project works under the hood? <a href='https://github.com/sandy-sp/art-of-data-analysis/tree/main/projects/earthquake_viz/README.md' target='_blank'>Check out the GitHub README</a> for code, setup, and deployment tips.
+    </p>
+    <p style='margin-top: 20px; font-size: 16px;'>
+        💡 Enjoying this project? Interested in building similar data apps or collaborating on earthquakes and visualization tools?
+        Let’s connect and share ideas!
+    </p>
+    <style>
+    .social-icons a {
+        text-decoration: none;
+        font-size: 20px;
+        display: inline-flex;
+        align-items: center;
+        margin-right: 10px;
+        margin-bottom: 6px;
+    }
+    .social-icons a:hover {
+        text-decoration: underline;
+    }
+    </style>
+    <div class='social-icons'>
+        <a href="https://www.linkedin.com/in/sandeep-paidipati" target="_blank">
+            <img src="https://cdn.jsdelivr.net/npm/simple-icons@v9/icons/linkedin.svg" width="24" style="margin-right:8px; vertical-align:middle;" /> LinkedIn
+        </a><br>
+        <a href="https://github.com/sandy-sp" target="_blank">
+            <img src="https://cdn.jsdelivr.net/npm/simple-icons@v9/icons/github.svg" width="24" style="margin-right:8px; vertical-align:middle;" /> GitHub
+        </a>
+    </div>
+    """, unsafe_allow_html=True)
+    st.markdown("</div>", unsafe_allow_html=True)
