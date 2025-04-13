f12a29d1f915350b0abc718f785b53de9edb2f12
sandy-sp <sandeep.paidipati@gmail.com>
2025-04-13 19:13:32 -0400

    Refactor display_correlation_analysis to accept data_bundle instead of user_inputs

--- projects/weather-quake-dashboard/src/pages/correlations.py
+++ projects/weather-quake-dashboard/src/pages/correlations.py
@@ -1,29 +1,13 @@
 import streamlit as st
-from src.api.open_meteo_api import fetch_historical_weather
-from src.api.usgs_earthquake_api import fetch_earthquake_data
 from src.utils.data_processing import align_weather_quake_data
 from src.utils.visualization import plot_magnitude_vs_weather
 
 
-def display_correlation_analysis(user_inputs):
+def display_correlation_analysis(data_bundle):
     st.subheader("🔍 Weather vs. Earthquake Correlation")
 
-    with st.spinner("Merging weather and earthquake data..."):
-        hourly_df, _ = fetch_historical_weather(
-            user_inputs['latitude'],
-            user_inputs['longitude'],
-            str(user_inputs['start_date']),
-            str(user_inputs['end_date'])
-        )
-
-        quake_df = fetch_earthquake_data(
-            starttime=str(user_inputs['start_date']),
-            endtime=str(user_inputs['end_date']),
-            min_magnitude=user_inputs['min_magnitude'],
-            latitude=user_inputs['latitude'],
-            longitude=user_inputs['longitude'],
-            limit=user_inputs['limit']
-        )
+    hourly_df = data_bundle["weather"]
+    quake_df = data_bundle["earthquakes"]
 
     if hourly_df.empty:
         st.warning("No weather data available for the selected location and time range.")
