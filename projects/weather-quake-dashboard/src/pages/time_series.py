dcd02ff75ca1721f017d1d52ff3c989058236f2b
sandy-sp <sandeep.paidipati@gmail.com>
2025-04-13 19:13:43 -0400

    Refactor display_timeseries to use data_bundle instead of user_inputs for improved data handling

--- projects/weather-quake-dashboard/src/pages/time_series.py
+++ projects/weather-quake-dashboard/src/pages/time_series.py
@@ -1,28 +1,12 @@
 import streamlit as st
-from src.api.open_meteo_api import fetch_historical_weather
-from src.api.usgs_earthquake_api import fetch_earthquake_data
 from src.utils.visualization import plot_temperature_humidity, plot_earthquake_frequency
 
 
-def display_timeseries(user_inputs):
+def display_timeseries(data_bundle):
     st.subheader("📈 Time Series Plots")
 
-    with st.spinner("Loading weather and earthquake data..."):
-        hourly_df, _ = fetch_historical_weather(
-            user_inputs['latitude'],
-            user_inputs['longitude'],
-            str(user_inputs['start_date']),
-            str(user_inputs['end_date'])
-        )
-
-        quake_df = fetch_earthquake_data(
-            starttime=str(user_inputs['start_date']),
-            endtime=str(user_inputs['end_date']),
-            min_magnitude=user_inputs['min_magnitude'],
-            latitude=user_inputs['latitude'],
-            longitude=user_inputs['longitude'],
-            limit=user_inputs['limit']
-        )
+    hourly_df = data_bundle["weather"]
+    quake_df = data_bundle["earthquakes"]
 
     if hourly_df.empty:
         st.warning("No weather data available for the selected location and time range.")
