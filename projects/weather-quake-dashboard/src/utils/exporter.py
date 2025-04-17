a499c400aa2884647c6a01356e38938d3496dc3b
sandy-sp <sandeep.paidipati@gmail.com>
2025-04-17 04:04:30 -0400

    Add function to export earthquake and boundary data as GeoJSON FeatureCollection

--- projects/weather-quake-dashboard/src/utils/exporter.py
+++ projects/weather-quake-dashboard/src/utils/exporter.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import io
+import geopandas as gpd
+
 
 def export_dataframe_as_excel(df: pd.DataFrame) -> bytes:
     output = io.BytesIO()
@@ -7,8 +9,35 @@ def export_dataframe_as_excel(df: pd.DataFrame) -> bytes:
         df.to_excel(writer, index=False, sheet_name='Sheet1')
     return output.getvalue()
 
+
 def export_dataframe_as_csv(df: pd.DataFrame) -> str:
     return df.to_csv(index=False)
 
+
 def export_dataframe_as_html(df: pd.DataFrame) -> str:
     return df.to_html(index=False)
+
+
+def export_quakes_and_boundaries_geojson(quake_df: pd.DataFrame, boundary_gdf: gpd.GeoDataFrame) -> str:
+    """
+    Merge earthquake points and tectonic boundary lines into a single GeoJSON FeatureCollection.
+    """
+    features = []
+
+    if not quake_df.empty:
+        quake_gdf = gpd.GeoDataFrame(
+            quake_df.copy(),
+            geometry=gpd.points_from_xy(quake_df['Longitude'], quake_df['Latitude']),
+            crs="EPSG:4326"
+        )
+        features.extend(quake_gdf.__geo_interface__["features"])
+
+    if boundary_gdf is not None and not boundary_gdf.empty:
+        features.extend(boundary_gdf.__geo_interface__["features"])
+
+    geojson = {
+        "type": "FeatureCollection",
+        "features": features
+    }
+
+    return io.StringIO(str(pd.io.json.dumps(geojson))).getvalue()
