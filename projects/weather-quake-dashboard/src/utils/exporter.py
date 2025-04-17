535a99e873515af2eb76b1e53fdc887ab9abf4f0
sandy-sp <sandeep.paidipati@gmail.com>
2025-04-16 23:56:46 -0400

    Refactor exporter functions to streamline DataFrame export methods and remove unused Plotly functions

--- projects/weather-quake-dashboard/src/utils/exporter.py
+++ projects/weather-quake-dashboard/src/utils/exporter.py
@@ -1,14 +1,14 @@
 import pandas as pd
-import plotly.io as pio
-import streamlit as st
+import io
 
-def export_plotly_to_html(fig, filename):
-    pio.write_html(fig, filename)
+def export_dataframe_as_excel(df: pd.DataFrame) -> bytes:
+    output = io.BytesIO()
+    with pd.ExcelWriter(output, engine='xlsxwriter') as writer:
+        df.to_excel(writer, index=False, sheet_name='Sheet1')
+    return output.getvalue()
 
-def export_plotly_to_png(fig, filename):
-    fig.write_image(filename)
+def export_dataframe_as_csv(df: pd.DataFrame) -> str:
+    return df.to_csv(index=False)
 
-def export_dataframe_to_excel(df: pd.DataFrame, filename: str):
-    with pd.ExcelWriter(filename, engine="xlsxwriter") as writer:
-        df.to_excel(writer, index=False)
-    return filename
+def export_dataframe_as_html(df: pd.DataFrame) -> str:
+    return df.to_html(index=False)
